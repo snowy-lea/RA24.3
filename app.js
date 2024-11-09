3f1af01551a5f07c08a41932164b8087802860a7
BuildTools <unconfigured@null.spigotmc.org>
2024-11-08 19:26:21 -0500

    index html added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app     = express();
 
-app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!'});
